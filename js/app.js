e9b077cd1d289b8e0a38e63914ae1846845b5c4a
Rohan <rohanharris1@outlook.com>
2020-10-19 13:34:58 +0100

    portfolio made and styling added

---
+++ js/app.js
@@ -0,0 +1,34 @@
+((d,w) => {
+    d.addEventListener('mousedown', function() {
+        var parent = d.querySelector('.splitview'),
+            topPanel = parent.querySelector('.top'),
+            handle = parent.querySelector('.handle'),
+           
+            delta = 0;
+            
+
+    
+
+        parent.addEventListener('mousemove', function(event) {
+             
+            
+            // Get the delta between the mouse position and center point.
+            delta = (event.clientX - w.innerWidth / 2) * 0.5;
+
+            // Move the handle.
+            handle.style.left = event.clientX + delta + 'px';
+
+            // Adjust the top panel width.
+            topPanel.style.width = event.clientX  + delta + 'px';
+   
+        });
+
+      
+    });
+
+   
+
+}) (document, window);
+
+
+
